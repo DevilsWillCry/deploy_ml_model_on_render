eee2c8595f6244095792a9895fc2af0dae36cbd0
Miguel Angel <miguel.delta21@gmail.com>
2025-04-21 17:08:01 -0500

    First Commit - Testing Server on FastAPI

---
+++ main.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI
+import pandas as pd 
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"message":"Servidor Funcionando✅"}
+
+
+
+@app.get("/create_csv")
+def create_csv():
+    datos = {
+        "nombre": ["John", "Jane", "Bob"],
+        "edad": [25, 30, 35],
+        "ciudad": ["New York", "London", "Paris"]
+    }
+
+    df = pd.DataFrame(datos)
+    df.to_csv("usuarios.csv", index=False)
+    return {"message": "CSV creado con éxito"}
