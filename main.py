1ecd44aa4b7e4e7a01824b93400deda6e8215a5b
Miguel Angel <miguel.delta21@gmail.com>
2025-04-24 00:59:52 -0500

    Fail deploy on render, fixin data taked from sensor/medicion_1

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ if not firebase_admin._apps:
     initialize_app(cred, {
         'databaseURL': 'https://esp32-thesis-project-default-rtdb.firebaseio.com/'
     })
-    ref = db.reference("/sensor/data/medicion_1")
+    ref = db.reference("/sensor/medicion_1")
     data = ref.get()
 
 if data != {}:
